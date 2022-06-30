aa0ace331941f85b0fdf885030d2f4dc81f3ead9
Evgeny Vasilev <vasilev@surf.dev>
2022-06-30 22:23:41 +0400

    First commit

---
+++ docTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  docTest
+//
+//  Created by Евгений Васильев on 30.06.2022.
+//
+
+import UIKit
+
+/// Some class with some methods
+public class ViewController: UIViewController {
+    
+    /// Some properties
+    public var someProperties: String = ""
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    /// This metod so somethink
+    public func someMethods() {}
+
+}
+
